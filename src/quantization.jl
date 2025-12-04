592371db88c498ae9e09dcdc7852c5dafe4c0010
obluda2173 <obluda2173@gmail.com>
2025-12-04 13:07:58 +0100

    quantization functions done

---
+++ src/quantization.jl
@@ -0,0 +1,33 @@
+# Standard JPEG Luminance Quantization Table
+LUMINANCE_TABLE = [
+    16 11 10 16 24 40 51 61;
+    12 12 14 19 26 58 60 55;
+    14 13 16 24 40 57 69 56;
+    14 17 22 29 51 87 80 62;
+    18 22 37 56 68 109 103 77;
+    24 35 55 64 81 104 113 92;
+    49 64 78 87 103 121 120 101;
+    72 92 95 98 112 100 103 99
+]
+
+# Standard JPEG Chrominance Quantization Table
+CROMINANCE_TABLE = [
+    17 18 24 47 99 99 99 99;
+    18 21 26 66 99 99 99 99;
+    24 26 56 99 99 99 99 99;
+    47 66 99 99 99 99 99 99;
+    99 99 99 99 99 99 99 99;
+    99 99 99 99 99 99 99 99;
+    99 99 99 99 99 99 99 99;
+    99 99 99 99 99 99 99 99
+]
+
+function quantize(dct_block, table)
+    scaled = dct_block ./ table
+    return Int.(round.(scaled))
+end
+
+function dequantize(quant_block, table)
+    dct_block = quant_block .* table
+    return dct_block
+end
